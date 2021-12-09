747b2a2f32ff24ecd9dc0a080c918ac234068f70
LorenzoRegalzi <lorenzo.regalzi@gmail.com>
2021-12-09 22:16:22 +0100

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static float fahrenheitToCelsius(float temperatureFahrenheit)
+        {
+            var temperatureCelsius = (temperatureFahrenheit - 32) / 1.8f;
+            return temperatureCelsius;
+        }
+    }
+}
